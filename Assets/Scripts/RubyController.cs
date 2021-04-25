b44f176cc0bb2340b63aeee1569f324ef7451b32
hktrollgames <hktrollgames@gmail.com>
2021-04-25 17:26:56 +0900

    루비에 스피드 적용

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,20 +4,56 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-    // Start는 처음 시작되는 부분 - Git테스트중
-    void Start()
+    private void Start()
     {
-        
+        //QualitySettings.vSyncCount = 0;
+        //Application.targetFrameRate = 10;
     }
-
-    // Update is called once per frame
+    public float speed = 5f;
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        Debug.Log(horizontal);
+        //Input -> 모르는 클래스 등장 "Unity Input"검색(네이버 검색보다 구글 검색)
+        // 매뉴얼 페이지 확인, 엄청 많은 API중 한개 -> 많은 API를 모두 외울 수 없다. 
+        // 사전 처럼 그때 그때 필요한것들을 참고해서 개발하자 -> 샘플 코드만 보고 적용.
+        // 예상한바로 작동안하면 매뉴얼 정독
+        // https://docs.unity3d.com/kr/current/ScriptReference/Input.html 
+        // https://docs.unity3d.com/kr/530/ScriptReference/Input.html
+
+        //Input.GetKey      // 키를 누르고 있을때 
+        //Input.GetKeyDown  // 키를 눌렀을때
+        //Input.GetKeyUp    // 누른 키를 땠을때
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            Debug.Log("숫자 1을 눌렀다");
+
+
+        float horizontal; 
+        float vertical;
+
+        // 방법1이나 2중 한개만 있으면 됨.
+        // 방법1 : 축 이용방법 ( Project setting -> Input Manager Axis에 값을 등록하여 사용, 에디터 설정도 참고하여야 해서 불편
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        // 방법 2 : 키코드를 직접 입력하여 키입력 감지. 부드러운 움직임 제거
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+            horizontal = -1;
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+            horizontal = 1;
+
+
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+            vertical = 1;
+
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+            vertical = -1;
+        // 방법 2 끝.
+
+        //Debug.Log(horizontal);
 
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
